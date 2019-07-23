b4bc956ea6e6e38527851f1738baf7ab2c0af7b1
zepotatomaster <chriswp5@yahoo.com>
2019-07-23 13:33:30 -0700

    Made sure text is empty

--- parse.py
+++ parse.py
@@ -1,43 +1,38 @@
 
 from pptx import Presentation
-from pptx.enum.shapes import MSO_SHAPE_TYPE
 
-prs = Presentation("C:/Users/siwei/Downloads/test.pptx")
-
-text_runs = []
-image_runs = []
+prs = Presentation("C:/Users/Administrator/Desktop/Hackathon/Sample.pptx")
 slide_list = []
+
 for slide in prs.slides:
-    for shape in slide.shapes:
-        if not shape.has_text_frame:
-            continue
-        #paras=shape.text_frame.paragraphs
-        #for paragraph in shape.text_frame.paragraphs:
-         #   for run in paragraph.runs:
-          #      text_runs.append(run.text)
-        len_1=len(shape.text_frame.paragraphs)
-        runs=''
-        for i in range(0,len_1):
-            for run in shape.text_frame.paragraphs[i].runs:
-                text_runs.append(run.text)
-        slide_list.append(text_runs)
-        text_runs=[]
+	elements = []
+	for shape in slide.shapes:
+		if shape.has_text_frame:
+			for para in shape.text_frame.paragraphs:
+				if para.text == "":
+					continue
+				elif shape.is_placeholder:
+					elements.append( ( True, shape.placeholder_format.type, para.text ) )
+				else:
+					elements.append( ( False, shape.shape_type, para.text) )
+	if len(elements) != 0:
+		slide_list.append(elements)
+print(slide_list)
             
-for slide in prs.slides:
-    for shape in slide.shapes:
-        if 'Picture' in shape.name:
-            picture = shape
-            image = picture.image
-            image_file_bytes = image.blob
-            file_extension = image.ext
-            file_path=image.filename
-            #print(file_path)
+# for slide in prs.slides:
+#     for shape in slide.shapes:
+#         if 'Picture' in shape.name:
+#             picture = shape
+#             image = picture.image
+#             image_file_bytes = image.blob
+#             file_extension = image.ext
+#             file_path=image.filename
+#             print(file_path)
 
  
 
-for shape in slide.shapes:
-    if shape.shape_type != MSO_SHAPE_TYPE.PICTURE:
-        continue
-    picture = shape
-    print(picture._pic.nvPicPr.cNvPr.get('descr'))
-print(slide_list)
+# for shape in slide.shapes:
+#     if shape.shape_type != MSO_SHAPE_TYPE:
+#         continue
+#     picture = shape
+#     print(picture._pic.nvPicPr.cNvPr.get('descr'))
