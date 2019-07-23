78b2ce0069027baf66562147f149a3a5526358cf
siweiw <siweiw@umich.edu>
2019-07-23 12:47:26 -0700

    Working no textboxes

--- parse.py
+++ parse.py
@@ -2,18 +2,16 @@ from pptx import Presentation
 from pptx.enum.shapes import PP_PLACEHOLDER
 
 prs = Presentation("C:/Users/siwei/Downloads/test.pptx")
+slide_list = []
 
-
-
-i = 1
 for slide in prs.slides:
-	print(i)
-	i = i + 1
+	objects = []
 	for shape in slide.placeholders:
 		if not shape.has_text_frame:
 			continue
 		if not shape.is_placeholder:
 			continue
 		for para in shape.text_frame.paragraphs:
-			print(shape.placeholder_format.type)
-			print(para.text)
+			objects.append( (shape.placeholder_format.type, para.text) )
+	if len(objects) != 0:
+		slide_list.append(objects)
