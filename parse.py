fd7dcd9ef90eb211c0e5c35791c2cfa83accd620
siweiw <siweiw@umich.edu>
2019-07-23 12:48:29 -0700

    Renaming

--- parse.py
+++ parse.py
@@ -5,13 +5,13 @@ prs = Presentation("C:/Users/siwei/Downloads/test.pptx")
 slide_list = []
 
 for slide in prs.slides:
-	objects = []
+	elements = []
 	for shape in slide.placeholders:
 		if not shape.has_text_frame:
 			continue
 		if not shape.is_placeholder:
 			continue
 		for para in shape.text_frame.paragraphs:
-			objects.append( (shape.placeholder_format.type, para.text) )
-	if len(objects) != 0:
-		slide_list.append(objects)
+			elements.append( (shape.placeholder_format.type, para.text) )
+	if len(elements) != 0:
+		slide_list.append(elements)
