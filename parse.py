c7a26603456e3ee17992c78e7cba8dd8c0428a8b
siweiw <siweiw@umich.edu>
2019-07-23 14:08:17 -0700

    Documentation

--- parse.py
+++ parse.py
@@ -1,5 +1,13 @@
 from pptx import Presentation
 
+# The returned array is two dimensional. The first dimension
+# indexes into the slide deck. The second dimension accesses
+# individual elements. Each element consists of 3 objects.
+# If elem[0] is true, then the object is a placeholder and elem[1]
+# is the placeholder_format enum type of the object. If elem[0] is
+# false, then the object is not a placeholder and elem[1] just
+# returns the general shape_type enum. In either case enum[2] is the
+# string containing the text in that element.
 def returnText(fileName):
 
 	prs = Presentation(fileName)
