96274ffadc3d6880f755bb7f77ef8d5cc92ddf29
vishshukla <vwshukla@gmail.com>
2019-07-24 11:44:24 -0700

    added the final version with summarizer.py, this adds ML that summarizes an essay or a passage

---
+++ summarizer.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division, print_function, unicode_literals
+
+from sumy.parsers.html import HtmlParser
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+
+
+LANGUAGE = "english"
+SENTENCES_COUNT = 5
+
+
+if __name__ == "__main__":
+    # url = "https://en.wikipedia.org/wiki/Automatic_summarization"
+    
+    # this is where you would put the file name with all the content you would want to summarize.
+    parser = PlaintextParser.from_file("content.txt", Tokenizer(LANGUAGE))
+    stemmer = Stemmer(LANGUAGE)
+
+    summarizer = Summarizer(stemmer)
+    summarizer.stop_words = get_stop_words(LANGUAGE)
+
+    for sentence in summarizer(parser.document, SENTENCES_COUNT):
+        print(sentence)
